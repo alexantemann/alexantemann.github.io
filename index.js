5cfaff2e40617d15f1e13e7093e5cc7018d002ba
alexantemann <115371450+alexantemann@users.noreply.github.com>
2025-01-20 16:56:31 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 
-// masonry
+//----------- masonry
 
 const VRES= 5; // rem: must match css grid-template-rows TODO? read css
 const VGAP= 40;
 const gallery= document.getElementById("gallery");
 const heightMap= new Map();
+
 function updateMasonry() {
+	for(let [elt,h] of heightMap.entries()) {
+		if(elt.hidden) continue;
+			h+= VGAP;
+		let span= (h/VRES)|0;
+		elt.style.gridRow= "span "+ span;
+		elt.style.height= (span*VRES)+"px";
+	}
+}
+function checkMasonry() {
 	let update= false;
 	for(let elt of gallery.children) {
 		if(elt.hidden) continue;
@@ -16,31 +26,47 @@ function updateMasonry() {
 			}
 	}
 	if(!update) return;
-	for(let [elt,h] of heightMap.entries()) {
-		if(elt.hidden) continue;
-			h+= VGAP;
-		let span= (h/VRES)|0;
-		elt.style.gridRow= "span "+ span;
-		elt.style.height= (span*VRES)+"px";
-	}
+	requestAnimationFrame(updateMasonry);
 }
-new ResizeObserver(updateMasonry).observe(gallery);
-window.addEventListener("load",updateMasonry);
 
-// filter
+new ResizeObserver(checkMasonry).observe(gallery);
+window.addEventListener("load",checkMasonry);
+
+//----------- filter
 
 const filterBar= document.getElementById("filters");
-const filterRadioByValue= Array.prototype.reduce.call(
-	filterBar.querySelectorAll("input"),
-	(map,input)=>(map.set(input.value,input),map),
+const filterLinkByHash= Array.prototype.reduce.call(
+	filterBar.querySelectorAll("a"),
+	(map,a)=>(map.set(a.getAttribute("href").substring(2),a),map),
 	new Map()
 );
 
-filterBar.addEventListener("change", (event) => {
-		window.location= "#-"+event.target.value;
-	});
+let currentFilteLink= filterLinkByHash.get(filter= "all");
 
-// navigation
+function updateFilter(filter) {
+	let scrollY= main.scrollTop;
+	currentFilteLink?.classList.remove("current");
+	currentFilteLink= (filterLinkByHash.get(filter)??filterLinkByHash.get(filter= "all"));
+	currentFilteLink.classList.add("current");
+
+	for(const item of gallery.children) {
+		item.hidden = filter !== "all" &&
+			!Array.prototype.find.call(
+				item.querySelectorAll("span.tag"),
+				(span) => span.innerText===filter
+			);
+	}
+	main.scrollTop= scrollY;
+	gallery.scrollIntoView({behavior: 'smooth'});
+}
+
+//----------- dark mode
+
+const darkMode= document.getElementById("dark-mode");
+darkMode.checked= window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+
+//----------- popup
 
 const popup= document.getElementById("popup");
 const iframe= document.getElementById("popup-iframe");
@@ -48,29 +74,36 @@ const nextLink= document.getElementById("next");
 const prevLink= document.getElementById("prev");
 const closeLink= document.getElementById("popup-close");
 
-//let scrollY= 0;
+// prevent history
+function replaceLocationOnClick(ev) {
+	ev.preventDefault();
+	ev.cancelBubble= true;
+	window.location.replace(ev.target.href);
+}
 
-function updatePopupState() {
-	let h= window.location.hash;
-	if(h.startsWith("#-")) {
-		iframe.contentWindow.location.replace("about:blank");
+nextLink.addEventListener("click",replaceLocationOnClick);
+prevLink.addEventListener("click",replaceLocationOnClick);
+closeLink.addEventListener("click",(ev)=>{
+		ev.preventDefault();
 		popup.close();
+});
 
-		document.getElementById("projects").scrollIntoView();
-		let filter= decodeURI(h.substring(2));
-		(filterRadioByValue.get(filter)??filterRadioByValue.get(filter= "all")).checked= true;
+let doBackOnPopupClose= false;
 
-		for(const item of gallery.children) {
-			item.hidden = filter !== "all" &&
-				!Array.prototype.find.call(
-					item.querySelectorAll("span.tag"),
-					(span) => span.innerText===filter
-				);
-		}
+gallery.addEventListener("click",ev=>{
+		doBackOnPopupClose= true;
+});
+
+popup.addEventListener("close", (ev) => {
+	iframe.contentWindow.location.replace("about:blank");
+	if(window.location.hash.startsWith("#+")) {
+		if(doBackOnPopupClose) window.history.back();
+		else window.location= "#"
 	}
-	else if(h.startsWith("#+")) {
-		//scrollY= window.scrollY;
-		const hrefs= Array.prototype.map.call(document.querySelectorAll("#gallery > div:not([hidden]) > a"),e=>e.href);
+});
+
+function openDetail() {
+		const hrefs= Array.prototype.map.call(gallery.querySelectorAll("div:not([hidden]) > a"),e=>e.href);
 		const i= hrefs.indexOf(window.location.toString());
 		prevLink.hidden= i===0;
 		if(i>0) {
@@ -80,33 +113,55 @@ function updatePopupState() {
 		if(i<hrefs.length-1) {
 			nextLink.href= hrefs[i+1];
 		}
-		closeLink.href= "#-" + filterBar.querySelector(`input:checked`).value;
-		iframe.contentWindow.location.replace(h.substring(2));
+		closeLink.href= currentFilteLink.href;
+		iframe.contentWindow.location.replace(window.location.hash.substring(2)+"#"+(darkMode.checked?"dark":"light"));
 		popup.showModal();
+}
+
+//----------- url hash
+
+function onLocationChange(ev) {
+	let h= window.location.hash;
+	console.log("onLocationChange",{ev,h})
+	if(!h.startsWith("#+")) {
+		popup.close();
+		if(h.startsWith("#-")) updateFilter(h.substring(2));
+		else document.getElementById(h.substring(1))?.scrollIntoView({behavior: 'smooth'});
 	}
-	else if(h.startsWith("#")) {
-		document.getElementById(h.substring(1))?.scrollIntoView();
+	else {
+		openDetail();
 	}
 }
 
-window.addEventListener('popstate', updatePopupState);
+window.addEventListener('popstate',()=>{
+	let h= window.location.hash;
+	if(h.startsWith("#+")) {
+		openDetail();
+	}
+	else {
+		popup.close();
+	}
+});
 
-popup.addEventListener("close", (event) => {
-		window.location= "#-" + filterBar.querySelector(`input:checked`).value;
-	});
+window.addEventListener("load",()=>{
+	let h= window.location.hash;
+	if(h.startsWith("#+")) {
+		openDetail();
+	}
+	else {
+		if(h.startsWith("#-")) updateFilter(h.substring(2));
+		else document.getElementById(h.substring(1))?.scrollIntoView({behavior: 'smooth'});
+		window.location.replace("#");
+	}
+});
 
-updatePopupState();
 
 
-/* section tracking / menu animation */
+//----------- section tracking
 
 const main = document.querySelector("main");
 const sections= main.querySelectorAll("section");
 const menu = document.getElementById("menu");
-const alex = document.getElementById("alex");
-const mybutton = document.getElementById("button-scroll");
-const menuColor = document.getElementById("nav");
-
 
 const visibleIds= {}
 
@@ -117,43 +172,40 @@ const sectionObserver = new IntersectionObserver(
 		}
 		let firstVisibleId= Array.prototype.find.call(sections,section=>visibleIds[section.id])?.id;
 		for(const link of menu.children) {
-			link.classList.toggle("current-section", link.href.endsWith('#'+firstVisibleId));
+			link.classList.toggle("current", link.href.endsWith('#'+firstVisibleId));
 		}
-		alex.classList.toggle("visible",firstVisibleId!=="home");
-
-
-		if (firstVisibleId === "home") {
-			mybutton.style.opacity = "0%";
-			mybutton.style.transform = "translateY(50px)";
-			menuColor.style.backgroundColor = "transparent";
-		  } else {
-			mybutton.style.opacity = "100%";
-			mybutton.style.transform = "translateY(0px)";
-			menuColor.style.backgroundColor = "var(--bg-color)";
-		  }
-
-
-
+		document.body.dataset.section= firstVisibleId;
 	},
-	{root:main, rootMargin:"-25% 0px 0px 0px"}
+	{root:main, rootMargin:"-15% 0px 0px 0px"}
 );
 
-for(const section of sections) {
-	sectionObserver.observe(section)
-}
+window.addEventListener("DOMContentLoaded",()=>{
+		for(const section of sections) {
+			sectionObserver.observe(section)
+		}
+});
 
-/* trick to give time to download fonts */
+//----------- local navigation override
+
+main.addEventListener("click",ev=>{
+	let target= ev.target.closest("a");
+	if(!target) return;
+	let link= target.getAttribute("href");
+	if(!link.startsWith("#")) return;
+
+	if(link.startsWith("#-")) {
+		ev.preventDefault();
+		updateFilter(link.substring(2));
+	}
+	else if(!link.startsWith("#+")) {
+		ev.preventDefault();
+		let anchor= document.getElementById(link.substring(1));
+		anchor?.scrollIntoView({behavior: 'smooth'});
+	}
+})
+
+//----------- trick to give time to download fonts
+// TODO replace with anim in css
 
 setTimeout(()=>(document.body.style.opacity=null),200)
 
-
-
-
-
-
-// When the user clicks on the button, scroll to the top of the <main> element
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-  mainElement.scrollTop = 0;
-}
